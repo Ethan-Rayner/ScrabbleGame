42ae4c4f3f7db5595d26cf7c978547bcde2d6160
JustPhilipMo <88906355+JustPhilipMo@users.noreply.github.com>
2022-05-08 15:58:17 +0800

    Update Tile.cpp

--- Tile.cpp
+++ Tile.cpp
@@ -1,4 +1,41 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include "Tiles.h"
+using namespace std;
 
-#include "Tile.h"
+Tile::Tile()
+{
+}
 
-// Empty... for now?
+Tile::~Tile()
+{
+}
+
+void Tile::S_Tile_file()
+{
+	ifstream file;
+	string buf;
+	file.open("scrabbletiles.txt", ios::in|ios::binary);
+	if (!file.is_open()) 
+	{
+		cout << "The file is opened fail" << endl;
+		return;
+	}
+	while (getline(file,buf)){}
+	file.close();
+}
+
+void Tile::S_Dictionary_file() {
+	ifstream file;
+	string buf;
+	file.open("twl06.txt", ios::in | ios::binary);
+	if (!file.is_open())
+	{
+		cout << "The file is opened fail" << endl;
+  }
+		return;
+	}
+	while (getline(file, buf)) {}
+	file.close();
+}
