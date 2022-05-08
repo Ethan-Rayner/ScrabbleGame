f8972e8b7890995c8e70af67d81067f8c1304782
s3680413 <s3680413@student.rmit.edu.au>
2022-05-08 13:11:29 +0930

    Create board

--- scrabble.cpp
+++ scrabble.cpp
@@ -1,9 +1,20 @@
 
 #include "LinkedList.h"
-
 #include <iostream>
+#include <vector>
+
 
 #define EXIT_SUCCESS 0
+#define COLUMNS      26
+#define ROWS         26
+#define CHAR    'A'
+
+using std::vector;
+using std::cout;
+using std::cin;
+using std::endl;
+
+void printBoard();
 
 int main(void)
 {
@@ -15,19 +26,20 @@ int main(void)
     * MAIN MENU
     */
    // Welcome Message
-   std::cout << "Welcome to Scrabble!\n-------------------\n"
-             << std::endl;
+   cout << "Welcome to Scrabble!\n-------------------\n"
+             << endl;
 
    // Main Menu Prompt
-   std::cout << "Menu\n----\n1. New Game\n2. Load Game\n3. Credits (Show student information\n4. Quit\n\n> "
+   cout << "Menu\n----\n1. New Game\n2. Load Game\n3. Credits (Show student information\n4. Quit\n\n> "
              << "";
-   std::cin >> selection;
-   std::cout << "" << std::endl;
+   cin >> selection;
+   cout << "" << endl;
 
    // Menu selection
    if (selection == NEW_GAME)
    {
       // NEW GAME CODE HERE
+      printBoard();
    }
    else if (selection == LOAD_GAME)
    {
@@ -35,22 +47,58 @@ int main(void)
    }
    else if (selection == CREDITS)
    {
-      std::cout << "----------------------------------" << std::endl;
-      std::cout << "Name: Shuo Zhou\nStudent ID: s3849606\nEmail: s3849606@student.rmit.edu.au\n" << std::endl;
-      std::cout << "Name: Ethan Rayner\nStudent ID: s3902240\nEmail: s3902240@student.rmit.edu.au\n" << std::endl;
-      std::cout << "Name: Joanna Tepaa\nStudent ID: s3680413\nEmail: s3680413@student.rmit.edu.au\n" << std::endl;
-      std::cout << "Name: Fei Mo\nStudent ID: s3754501\nEmail: s3754501@student.rmit.edu.au" << std::endl;
-      std::cout << "----------------------------------" << std::endl;
+      cout << "----------------------------------" << endl;
+      cout << "Name: Shuo Zhou\nStudent ID: s3849606\nEmail: s3849606@student.rmit.edu.au\n" << endl;
+      cout << "Name: Ethan Rayner\nStudent ID: s3902240\nEmail: s3902240@student.rmit.edu.au\n" << endl;
+      cout << "Name: Joanna Tepaa\nStudent ID: s3680413\nEmail: s3680413@student.rmit.edu.au\n" << endl;
+      cout << "Name: Fei Mo\nStudent ID: s3754501\nEmail: s3754501@student.rmit.edu.au" << endl;
+      cout << "----------------------------------" << endl;
    }
    else if (selection == EXIT)
    {
-      std::cout << "Goodbye" << std::endl;
+      cout << "Goodbye" << endl;
       return EXIT_SUCCESS;
    }
    else
    {
-      std::cout << "Sorry, that is not a valid option." << std::endl;
+      cout << "Sorry, that is not a valid option." << endl;
    }
 
    return EXIT_SUCCESS;
+}
+
+vector<vector<int> > board(ROWS, vector<int> (COLUMNS)) ;
+
+void printBoard(){
+
+
+    //printing out column
+  for(int column = 0; column < COLUMNS; column++){
+        cout << column;
+    }
+
+        cout << endl;
+        cout << "------------------------";
+        cout << "------------------------";
+        cout << endl;
+
+char rowTag = CHAR;
+
+//Printing out rows
+for (int row = 0; row < ROWS; row++) {
+        cout << rowTag << "  " << "|";     
+        rowTag++;
+
+        for (int column = 0; column < COLUMNS; column++) {
+            
+            if (board[row][column] == 0) { 
+                cout << "  ";
+            } else {
+                cout << board[row][column];  
+            }
+            cout << "|"; 
+        } 
+        cout << endl; 
+    }  
+     
 }
