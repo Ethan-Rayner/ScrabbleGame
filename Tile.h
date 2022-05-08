113771d8ecb44d5d61387682389e32028178c9d3
JustPhilipMo <88906355+JustPhilipMo@users.noreply.github.com>
2022-05-08 17:53:07 +0800

    Update Tile.h

--- Tile.h
+++ Tile.h
@@ -1,17 +1,22 @@
+#ifndef TILES         //Prevent multiple declarations
+#define TILES
 
-#ifndef ASSIGN2_TILE_H
-#define ASSIGN2_TILE_H
+#include <iostream>                 
+#include <string>     
+#include <fstream>
+using namespace std;
 
-// Define a Letter type
-typedef char Letter;
-
-// Define a Value type
-typedef int Value;
-
-class Tile {
+class Tile 
+{
 public:
-   Letter letter;
-   Value  value;
-};
 
-#endif // ASSIGN2_TILE_H
+	void S_Tile_file();
+	void S_Dictionary_file();
+
+
+private:
+	string buff;
+	char word_tiles;
+	int value;
+};
+#endif // TILES
