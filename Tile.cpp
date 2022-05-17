27e6fd5337f10cacd1dbcc89336cb894929fb368
Ethan-Rayner <s3902240@student.rmit.edu.au>
2022-05-17 15:34:43 +1000

    removed reading into tile class and dictionary file (not sure why they were there)

--- Tile.cpp
+++ Tile.cpp
@@ -24,31 +24,3 @@ char Tile::getLetter(){
 int Tile::getValue(){
 	return value;
 }
-
-void Tile::S_Tile_file()
-{
-	ifstream file;
-	string buf;
-	file.open("scrabbletiles.txt", ios::in|ios::binary);
-	if (!file.is_open()) 
-	{
-		cout << "The file is opened fail" << endl;
-		return;
-	}
-	while (getline(file,buf)){}
-	file.close();
-}
-
-void Tile::S_Dictionary_file() {
-	ifstream file;
-	string buf;
-	file.open("twl06.txt", ios::in | ios::binary);
-	if (!file.is_open())
-	{
-		cout << "The file is opened fail" << endl;
-  }
-		return;
-	
-	while (getline(file, buf)) {}
-	file.close();
-}
